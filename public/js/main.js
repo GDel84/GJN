568fa13eac467b9505d3e037e5a6b865fcbca92a
GDel84 <g-del@hotmail.fr>
2023-08-19 17:07:28 +0200

    corection bug js and mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,134 +1,108 @@
 jQuery(document).ready(function($) {
+  'use strict';
 
-	'use strict';
+  var owl = $("#owl-testimonials");
+  
+  owl.owlCarousel({
+    pagination: true,
+    paginationNumbers: false,
+    autoPlay: 6000, //Set AutoPlay to 3 seconds
+    items: 1,
+    itemsDesktop: [1000, 1],
+    itemsDesktopSmall: [900, 1],
+    itemsTablet: [600, 1],
+    itemsMobile: false
+  });
 
-      var owl = $("#owl-testimonials");
+  var top_header = $('.parallax-content');
+  top_header.css({ 'background-position': 'center center' });
 
-        owl.owlCarousel({
-          
-          pagination : true,
-          paginationNumbers: false,
-          autoPlay: 6000, //Set AutoPlay to 3 seconds
-          items : 1, //10 items above 1000px browser width
-          itemsDesktop : [1000,1], //5 items between 1000px and 901px
-          itemsDesktopSmall : [900,1], // betweem 900px and 601px
-          itemsTablet: [600,1], //2 items between 600 and 0
-          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-          
-      });
+  $(window).scroll(function() {
+    var st = $(this).scrollTop();
+    top_header.css({ 'background-position': 'center calc(50% + ' + (st * 0.5) + 'px)' });
+  });
 
+  $('.counter').each(function() {
+    var $this = $(this),
+      countTo = $this.attr('data-count');
 
-        var top_header = $('.parallax-content');
-        top_header.css({'background-position':'center center'}); // better use CSS
+    $({ countNum: $this.text() }).animate({
+      countNum: countTo
+    }, {
+      duration: 8000,
+      easing: 'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+    });
+  });
 
-        $(window).scroll(function () {
-        var st = $(this).scrollTop();
-        top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
-        });
+  // Fonction pour vérifier si l'écran est en mode mobile
+  function isMobile() {
+    return window.innerWidth <= 768;
+  }
 
-
-        $('.counter').each(function() {
-          var $this = $(this),
-              countTo = $this.attr('data-count');
-          
-          $({ countNum: $this.text()}).animate({
-            countNum: countTo
-          },
-
-          {
-
-            duration: 8000,
-            easing:'linear',
-            step: function() {
-              $this.text(Math.floor(this.countNum));
-            },
-            complete: function() {
-              $this.text(this.countNum);
-              //alert('finished');
-            }
-
-          });  
-          
-        });
-
-
-        $('.tabgroup > div').hide();
-        $('.tabgroup > div:first-of-type').show();
-        $('.tabs a').click(function(e){
-          e.preventDefault();
-            var $this = $(this),
-            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-            others = $this.closest('li').siblings().children('a'),
-            target = $this.attr('href');
-        others.removeClass('active');
-        $this.addClass('active');
-        $(tabgroup).children('div').hide();
-        $(target).show();
-      
-        })
-
-
-
-        $(".pop-button").click(function () {
-            $(".pop").fadeIn(300);
-            
-        });
-
-        $(".pop > span").click(function () {
-            $(".pop").fadeOut(300);
-        });
-
-
-        $(window).on("scroll", function() {
-            if($(window).scrollTop() > 100) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in your css)
-               $(".header").removeClass("active");
-            }
-        });
-
- // Fonction pour vérifier si l'écran est en mode mobile
-function isMobile() {
-  return window.innerWidth <= 768; // Vous pouvez ajuster cette valeur en fonction de vos besoins
-}
-
-$(document).ready(function() {
   if (isMobile()) {
-    // Code à exécuter en mode mobile
-    $('.tabgroup > div').show(); // Afficher toutes les div
-    $('.tabs a').click(function(e){
+    $('.tabgroup > div').show();
+    $('.tabs a').click(function(e) {
       e.preventDefault();
       var $this = $(this);
       $('.tabs a').removeClass('active');
       $this.addClass('active');
     });
   } else {
-    // Code à exécuter en mode non-mobile
     $('.tabgroup > div').hide();
     $('.tabgroup > div:first-of-type').show();
-    $('.tabs a').click(function(e){
+    $('.tabs a').click(function(e) {
       e.preventDefault();
       var $this = $(this),
-          tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-          others = $this.closest('li').siblings().children('a'),
-          target = $this.attr('href');
+        tabgroup = '#' + $this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('li').siblings().children('a'),
+        target = $this.attr('href');
       others.removeClass('active');
       $this.addClass('active');
       $(tabgroup).children('div').hide();
       $(target).show();
     });
   }
-});
 
+  $('.projects-holder').mixitup({
+    effects: ['fade', 'grayscale'],
+    easing: 'snap',
+    transitionSpeed: 400
+  });
 
-	/************** Mixitup (Filter Projects) *********************/
-    	$('.projects-holder').mixitup({
-            effects: ['fade','grayscale'],
-            easing: 'snap',
-            transitionSpeed: 400
+  $(".pop-button").click(function() {
+    $(".pop").fadeIn(300);
+  });
+
+  $(".pop > span").click(function() {
+    $(".pop").fadeOut(300);
+  });
+
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() > 100) {
+      $(".header").addClass("active");
+    } else {
+      $(".header").removeClass("active");
+    }
+  });
+
+  // Code pour désactiver les liens en mode mobile
+  document.addEventListener('DOMContentLoaded', function() {
+    var isMobile = window.innerWidth <= 768;
+    if (isMobile) {
+      var links = document.querySelectorAll('.clickable-link');
+      links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+          event.preventDefault();
         });
-
-
+      });
+    }
+  });
 
 });
+
